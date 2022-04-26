4a44dad0760725c44f5f92b67602d105adb3e86a
Pauliina Raitaniemi <pr222ja@student.lnu.se>
2022-04-26 19:17:55 +0200

    Add and fix search query

--- pages/api/coffee.js
+++ pages/api/coffee.js
@@ -1,4 +1,3 @@
-// 
 import { Client } from '@elastic/Elasticsearch'
 /**
  * Connect to the Elastic Search Client.
@@ -12,20 +11,83 @@ async function getESClient() {
     },
     auth: {
       username: process.env.ES_USERNAME,
-      password: process.env.PASSWORD
+      password: process.env.ES_PASSWORD
     }
   })
   
   return client
 }
 
+/**
+ * Searching for coffee results.
+ * 
+ * @param {*} req - request object.
+ * @param {*} res - response object.
+ */
 export default async function handler(req, res) {
   try {
     const client = await getESClient()
 
     let data = []
 
-    
+    const searchResult = await client.search({
+      index: 'coffee',
+      body: {
+        "aggs": {
+          "coffeeData": {
+            "terms": {
+              "field": "Country_of_Origin",
+              "order": {
+                "_key": "asc"
+              },
+              "size": 50
+            },
+            "aggs": {
+              "body_avg": {
+                "avg": {
+                  "field": "Body"
+                }
+              },
+              "acidity_avg": {
+                "avg": {
+                  "field": "Acidity"
+                }
+              },
+              "aftertaste_avg": {
+                "avg": {
+                  "field": "Aftertaste"
+                }
+              },
+              "cupperPoints_avg": {
+                "avg": {
+                  "field": "Cupper_Points"
+                }
+              }
+            }
+          }
+        },
+        "size": 0,
+        "fields": [],
+        "script_fields": {},
+        "stored_fields": [
+          "*"
+        ],
+        "runtime_mappings": {},
+        "_source": {
+          "excludes": []
+        },
+        "query": {
+          "range": {
+            "Cupper_Points": {
+              "gte": 7.5,
+              "lte": 11
+            }
+          }
+        }
+      }
+    })
+
+    data = searchResult.aggregations.coffeeData.buckets
 
     res.status(200).json({ data })
   } catch (error) {
